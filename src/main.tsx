f238254b6faa5d0c7daa3f839d6b0dc89c3530d7
hari13172 <haridiablo72@gmail.com>
2025-05-23 17:48:32 +0530

    feat: Add Toaster component for notifications in the main application

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router"
 import "./index.css"
 import App from "./App"
+import { Toaster } from "sonner"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster richColors position="top-right" />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
